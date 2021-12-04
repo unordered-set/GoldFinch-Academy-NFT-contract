d5162c1f60e20dd874fd08f7cc3363d5228727fe
Kostya N <UNORDERED_SET@TUTANOTA.COM>
2021-12-04 03:23:13 +0100

    Updated values

--- contracts/GoldFinchAcademyParticipantNFT.sol
+++ contracts/GoldFinchAcademyParticipantNFT.sol
@@ -35,7 +35,7 @@ contract GoldFinchAcademyParticipantNFT is ERC721 {
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
         require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
         if (tokenId / 1000000 == 1) return "https://gateway.pinata.cloud/ipfs/QmeqJtncwn4yA7Fpt1PYuWjQb3Sgjys89QsrBavVPyJfnA";
-        if (tokenId / 1000000 == 2) return "https://gateway.pinata.cloud/ipfs/QmZBhQTEq6dwS4kMBKneMQ3C6gR8PZtXx3aZEg3uyruCsr";
+        if (tokenId / 1000000 == 2) return "https://gateway.pinata.cloud/ipfs/QmUGLDsqRgUHxRoNwQeTKt9LmANroB59TDjpXesUxTfVqh";
         // We should not get below here, but let's say if we did, they are participants.
         return "https://gateway.pinata.cloud/ipfs/QmeqJtncwn4yA7Fpt1PYuWjQb3Sgjys89QsrBavVPyJfnA";
     }
